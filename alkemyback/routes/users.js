168245f1aba9a4ad0a8219483b9a70981e5b688e
nicoContigliani <nico.contigliani@gmail.com>
2021-11-26 19:08:10 -0300

    routesBack

--- alkemyback/routes/users.js
+++ alkemyback/routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+const users = require('./../apiservice/users/userRoutes')
+
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
+
+
+router.use('/users', users);
+
 
 module.exports = router;
