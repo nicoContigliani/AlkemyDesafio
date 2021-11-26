d471533a880844c9afa20d2d188ba11b15cc7b42
nicoContigliani <nico.contigliani@gmail.com>
2021-11-26 19:26:37 -0300

    loginback

--- alkemyback/apiservice/users/userModel.js
+++ alkemyback/apiservice/users/userModel.js
@@ -2,37 +2,7 @@ const usersDao = require('./userDao');
 const bcrypt = require('bcrypt')
 
 
-// module.exports = {
 
-// //   async getUsers(page, limit) {
-// //     return userDao.getUsers(page, limit);
-// //   },
-
-// async getUsers() {
-//   return usersDao.getUser();
-// },  
-// async getUser(id) {
-//     return usersDao.getUser(id);
-//   },
-
-//   async createUser(user) {
-//     return usersDao.createUser(user);
-//   },
-
-//   async updateUser(id, { email, username }) {
-//     return usersDao.updateUser(id, { email, username });
-//   },
-
-//   async deleteUser(id) {
-//     return usersDao.updateUser(id);
-//   },
-// };
-
-
-
-// async getUsers() {
-//   return usersDao.getUser();
-// }, 
 
 const register = async (elemento) => {
   const users = []
@@ -63,12 +33,18 @@ const login = async (elemento) => {
 
   const resultados = user.filter(u => u.fullname === fullname)
 
+
   if (resultados.length >= 1 && await bcrypt.compare(password, resultados[0].password)) {
     // console.log("hay elemento")
     // const validPassword = await bcrypt.compare(password, resultados[0].password);
     users.push(resultados)
   } else {
-    users.push("error contraseña o usuario")
+    users.push([[
+      {
+          "id_user": "0",
+          "fullname": "not user"
+      }
+  ]])
   }
   return users
 }
