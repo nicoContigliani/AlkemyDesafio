7dbc643c6109646b198caac03a9c5213a4899eb8
nicoContigliani <nico.contigliani@gmail.com>
2021-11-26 19:15:22 -0300

    loginback

--- alkemyback/apiservice/users/userRoutes.js
+++ alkemyback/apiservice/users/userRoutes.js
@@ -5,10 +5,10 @@ const router = express.Router();
  const user = require('./userController');
 
 
-//  router.get('/', user.gets);
-// //  router.get('/sigin', user.sigin);
-//  router.post('/register', user.register);
-//  router.post('/login', user.login);
+ router.get('/', user.gets);
+//  router.get('/sigin', user.sigin);
+ router.post('/register', user.register);
+ router.post('/login', user.login);
 
 
 
